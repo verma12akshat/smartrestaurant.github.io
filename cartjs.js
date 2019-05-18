b9cd073d80245a306c8578baebd8036666f479b5
verma12akshat <44532638+verma12akshat@users.noreply.github.com>
2019-05-18 11:28:21 +0530

    Update cartjs.js

--- cartjs.js
+++ cartjs.js
@@ -1,54 +1,88 @@
+var label = document.getElementsByClassName("name");
+var ul = document.querySelector("#dynamic-list");
+var value = document.getElementsByClassName("price");
+var un = document.querySelector("#dynamiclist2");
+let CartItems = [];
 
-// var input=document.getElementsByClassName('addToCart')
+function addToCartItem(event, whichValue) {
+  event.preventDefault();
 
-// console.log(label);
+  var itemName = label[whichValue].innerText;
 
-// for (var i = 0 ; i < input.length; i++){
-// //console.log(i);
-// var label_value = label[i].innerText;
-// input[i].addEventListener("click", function(i) {
-	
-//     
-//     // y= label.childNode[i];
-//     // z=y.nodeValue;
-    
-//     console.log(i);
+  var newCartObj = {
+    id: Math.random(),
+    data: {
+      name: itemName,
+      qty: 1,
+      price: 0,
+      addedOn: new Date().toLocaleString()
+    }
+  };
 
-//     
-//     
-//     
-    
-//   });
-//  }
+  var indx = CartItems.findIndex(dataObj => dataObj.data.name === itemName);
 
-var label=document.getElementsByClassName("name");
-var ul = document.querySelector('#dynamic-list');
-function addToCartItem(event,whichValue)
-{   
-	event.preventDefault();
-	console.log(label[whichValue].innerText);
-	
-	// var label=document.getElementsByClassName("");
-	var li =document.createElement("li");
-	var h=document.createTextNode(label[whichValue].innerText);
-	li.appendChild(h);
-	document.querySelector('#dynamic-list').appendChild(li);
-	console.log(h);
+  if (indx === -1) {
+    CartItems.push(newCartObj);
+    var price = value[whichValue].innerText;
+    RenderDOMCartItems(CartItems, price);
+  } else {
+    console.log(CartItems[indx]);
+    var name = CartItems[indx].data.name;
+    var qty = CartItems[indx].data.qty;
+    var price = value[whichValue].innerText;
+    var addedOn = CartItems[indx].data.addedOn;
+    CartItems = [
+      ...CartItems.splice(0, indx),
+      {
+        ...CartItems[indx],
+        id: newCartObj.id,
+        data: {
+          name: name,
+          qty: qty + 1,
+          price: price * (qty + 1),
+          addedOn: addedOn,
+          updatedOn: newCartObj.data.addedOn
+        }
+      },
+      ...CartItems.splice(indx + 1)
+    ];
+    console.log(CartItems[indx].data.qty);
+    // price = price * CartItems[indx].data.qty;
+    console.log(CartItems[indx].data.price);
 
-
+    // var z = document.querySelectorAll("#dynamic-list > li");
+    // z.forEach(li => {
+    //   if (li.innerText.substring(name)) {
+    //   }
+    // });
+  }
 }
 
-// price algorithm
+function remove(link) {
+  link.parentNode.parentNode.removeChild(link.parentNode);
+}
 
-var value=document.getElementsByClassName("price");
-var un=document.querySelector('#dynamiclist2');
-function addvalue(event,whichValue)
-{
-	event.preventDefault();
-	console.log(value[whichValue].innerText);
-	var l2i=document.createElement("li");
-	var x=document.createTextNode(value[whichValue].innerText);
-	l2i.appendChild(x);
-	document.querySelector('#dynamiclist2').appendChild(l2i);
-	console.log(x);
+function RenderDOMCartItems(CartItems, price) {
+  var li = document.createElement("li");
+  var removeIcon = document.createElement("span");
+  removeIcon.className += "removeclass";
+  var priceTag = document.createElement("span");
+  priceTag.className += "priceclass";
+  li.appendChild(
+    document.createTextNode(CartItems[CartItems.length - 1].data.name)
+  );
+
+  li.appendChild(removeIcon);
+  li.appendChild(priceTag);
+  removeIcon.setAttribute("onclick", `remove(this)`);
+  removeIcon.appendChild(document.createTextNode("X"));
+  CartItems[CartItems.length - 1].data.price = price;
+  // for (i = 0; i > CartItems.length; i++) {
+  priceTag.appendChild(
+    document.createTextNode(CartItems[CartItems.length - 1].data.price)
+  );
+  // }
+  // console.log(total);
+
+  document.querySelector("#dynamic-list").appendChild(li);
 }
